7e094c89400f5df9c2d7cc78635afdcaf5808736
Воронин Игорь Вячеславович <ivvoro15@mts.ru>
2023-11-23 13:55:00 +0300

    Добавлена выгрузка из MAPIC

--- app.mjs
+++ app.mjs
@@ -4,9 +4,13 @@ import { processGetSequenceResponse } from './api/sequence.mjs'
 
 const app = express();
 const port = 3000;
+const pathToSwaggerUi = require('swagger-ui-dist').absolutePath()
+
 
 app.use( '/messages', express.static( './view/sequence-details.html'))
 app.use( '/js', express.static( './view/js'))
+app.use( '/swagger-ui', express.static(pathToSwaggerUi));
+app.use( '/api/swagger/domain.yaml', express.static('./domain.yaml'));
 
 app.get('/', async (req, res) => {
     res.send('Hello, World!')
