434284933fb1752d216d6de8803e5494974146f2
Voronin Igor <IVvoronin@beeline.ru>
2024-02-05 12:21:01 +0300

    Настройка CICD переменных окружения

--- src/utils/ea-repo.mjs
+++ src/utils/ea-repo.mjs
@@ -1,33 +1,49 @@
 import pg from 'pg'
 
 const ENVIROMENT_VARIABLE = {
-    user : "DB_EA_USER", password : "DB_EA_PASSWORD", host: "DB_EA_URL", database: "DB_EA_DATABASE"
+    user: "DB_EA_USER", password: "DB_EA_PASSWORD", host: "DB_EA_URL", database: "DB_EA_DATABASE"
 }
 
 class Repository {
-    #config = {};
+    #config;
     constructor() {
-        for( const name in ENVIROMENT_VARIABLE){
-            if( !process.env[ENVIROMENT_VARIABLE[name]]){
+
+    }
+
+    get config() {
+        if (this.#config) return this.#config;
+
+        let config = {};
+        for (const name in ENVIROMENT_VARIABLE) {
+            if (!process.env[ENVIROMENT_VARIABLE[name]]) {
                 throw Error(`Environment variable [${ENVIROMENT_VARIABLE[name]}] not set`);
             }
-            this.#config[name] = process.env[ ENVIROMENT_VARIABLE[ name]];
+            config[name] = process.env[ENVIROMENT_VARIABLE[name]];
         }
+        if (config.host.startsWith('jdbc:')){
+            let url = new URL( config.host.slice(5) );
+            config.host = url.hostname;
+            /*
+            if( url.port ) config.port = url.port;
+            if( url.pathname ){
+                let paths = url.pathname.split('/');
+                if( paths.length > 1) config.database = paths[1];
+            }
+            */
+        }
+        this.#config = config;
+        return this.#config
     }
 
     async queryRows(sql) {
-        let client = new pg.Client(this.#config);
+        let client = new pg.Client(this.config);
         await client.connect();
         let rows = (await client.query(sql)).rows;
         client.end();
         return rows;
     }
-    async getObjectsByAlias( alias ){
-        let client = new pg.Client(this.#config);
-        await client.connect();
-        let rows = (await client.query({text:"select * from t_object where alias=$1", values: [alias] })).rows;
-        client.end();
-        return rows;
+    async getObjectsByAlias(alias) {
+        return this.queryRows({ text: "select * from t_object where alias=$1", values: [alias] });
     }
 }
 
