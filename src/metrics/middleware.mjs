0e6eae2c7bfe9c905cbeb6b6dc53cbe203ffd60d
Voronin Igor <IVvoronin@beeline.ru>
2024-04-25 15:26:28 +0300

    + api metrics

--- src/metrics/middleware.mjs
+++ src/metrics/middleware.mjs
@@ -5,19 +5,20 @@ import client from 'prom-client'
 const register = new client.Registry()
 
 register.setDefaultLabels({
-    app: 'ea-board'
-})
+    system: 'FDMSHOWCASEAPP', container: 'ea-board', interface: 'sparx-api'
+});
 
 let httpRequestDurationMicroseconds = new client.Histogram(
     {
         name: 'http_request_duration_seconds',
         help: 'Duration of HTTP requests in microseconds',
         labelNames: ['method', 'path', 'code'],
-
     })
 
 register.registerMetric(httpRequestDurationMicroseconds)
 
+
+//const paths = [ /a/ ]
 /**
  * 
  * @param {exporess.Request} req 
@@ -29,6 +30,10 @@ export default async function RESTMetric(req, res, next) {
         res.setHeader('Content-Type', register.contentType)
         res.send(await register.metrics());
         return;
+    }
+    
+    if (!req.path.startsWith('/api')) {
+        return next();
     }
     const end = httpRequestDurationMicroseconds.startTimer();
     await next();
