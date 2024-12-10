c807742512ab59dceb7cda4596cc5d3f7c863e18
Voronin Igor <IVvoronin@beeline.ru>
2024-12-10 16:21:03 +0300

    fix node 22

--- src/resources/const.mjs
+++ src/resources/const.mjs
@@ -1,4 +1,4 @@
-import packageJson from '../../package.json' assert { type: "json" };
+import packageJson from '../../package.json' with { type: "json" };
 
 
 export const API_VERSION = packageJson.version;
