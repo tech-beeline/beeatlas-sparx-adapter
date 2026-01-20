a484d6ea3e8755153e2a446c485ebe7a1a55be19
Igor Voronin <ivvoronin@beeline.ru>
2026-01-20 11:16:53 +0300

    - remove legacy

--- sql/composite-object.sql
+++
@@ -1,5 +0,0 @@
-select cd.object_id, d.diagram_id 
-from t_object cd
-	join t_object comp on comp.object_id=cd.classifier
-	join t_xref x on x.client=cd.ea_guid and x.name='DefaultDiagram'
-	join t_diagram d on d.ea_guid=x.supplier
--- sql/ref-object.sql
+++
@@ -1,4 +0,0 @@
-select ref.object_id, ref.pdata1
-from t_object mep 
-	join t_object ref on ref.object_id=mep.parentid
-where mep.object_type='MessageEndpoint'
